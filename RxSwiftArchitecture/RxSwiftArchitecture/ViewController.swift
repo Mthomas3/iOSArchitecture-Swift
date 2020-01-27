97a33a0b3a43512c2cae927b9ff6e0aa2ea4020b
Thomas Martins <thomas.martins@epitech.eu>
2020-01-27 21:12:25 +0100

    Setting up project

---
+++ RxSwiftArchitecture/RxSwiftArchitecture/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RxSwiftArchitecture
+//
+//  Created by Thomas Martins on 27/01/2020.
+//  Copyright © 2020 Thomas Martins. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
